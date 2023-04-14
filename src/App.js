3b5688f34cf28b1fad70bb7fbc407dc1efbbbfb2
chain <klaus.mika1052@gmail.com>
2023-04-14 16:40:43 -0300

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
