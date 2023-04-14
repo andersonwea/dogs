8964b40d06acc1df6993ad70ec9a70e1c1e6132a
chain <klaus.mika1052@gmail.com>
2023-04-14 18:22:25 -0300

    criado estilos de componentes de input e label para o formulario de login

--- src/Components/Forms/Input/Input.js
+++ src/Components/Forms/Input/Input.js
@@ -3,13 +3,17 @@ import styles from "./Input.module.css";
 
 const Input = ({ name, label, type, value, setValue }) => {
   return (
-    <div>
-      <label htmlFor={name}>{label}</label>
+    <div className={styles.wrapper}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
       <input
+        className={styles.input}
         type={type}
         value={value}
         onChange={({ target }) => setValue(target.value)}
       />
+      <p className={styles.error}>Error</p>
     </div>
   );
 };
