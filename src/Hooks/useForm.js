fe9d8ec231703699c4bcd8cba9a015e47b43ed28
chain <klaus.mika1052@gmail.com>
2023-04-16 17:03:41 -0300

    adicionado enviado de usuário criado via api POST

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,35 +1,35 @@
-import React from "react";
+import React from 'react'
 
 const types = {
   email: {
     regex:
       /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi,
-    message: "Preencha um email válido",
-  },
-};
+    message: 'Preencha um email válido'
+  }
+}
 
-const useForm = (type) => {
-  const [value, setValue] = React.useState("");
-  const [error, setError] = React.useState(null);
+const useForm = type => {
+  const [value, setValue] = React.useState('')
+  const [error, setError] = React.useState(null)
 
-  const validate = (value) => {
-    if (type === false) return true;
+  const validate = value => {
+    if (type === false) return true
     if (value.length === 0) {
-      setError("Preencha um valor.");
-      return false;
+      setError('Preencha um valor.')
+      return false
     } else if (types[type] && !types[type].regex.test(value)) {
-      setError(types[type].message);
-      return false;
+      setError(types[type].message)
+      return false
     } else {
-      setError(null);
-      return true;
+      setError(null)
+      return true
     }
-  };
+  }
 
   const handleChange = ({ target }) => {
-    if (error) validate(target.value);
-    setValue(target.value);
-  };
+    if (error) validate(target.value)
+    setValue(target.value)
+  }
 
   return {
     value,
@@ -37,8 +37,8 @@ const useForm = (type) => {
     handleChange,
     error,
     validate: () => validate(value),
-    onBlur: () => validate(value),
-  };
-};
+    onBlur: () => validate(value)
+  }
+}
 
-export default useForm;
+export default useForm
--- src/pages/Login/LoginCreate.js
+++ src/pages/Login/LoginCreate.js
@@ -2,14 +2,26 @@ import React from 'react'
 import Input from '../../Components/Forms/Input/Input'
 import useForm from '../../Hooks/useForm'
 import Button from '../../Components/Forms/Button/Button'
+import { USER_POST } from '../../services/api'
+import { UserContext } from '../../storage/UserContext'
 
 const LoginCreate = () => {
   const username = useForm()
-  const email = useForm()
+  const email = useForm('email')
   const password = useForm()
+
+  const { userLogin } = React.useContext(UserContext)
 
   const handleSubmit = async event => {
     event.preventDefault()
+    const { url, options } = USER_POST({
+      username: username.value,
+      email: email.value,
+      password: password.value
+    })
+    const response = await fetch(url, options)
+    if (response.ok) userLogin(username.value, password.value)
+    console.log(response)
   }
 
   return (
