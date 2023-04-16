9d37b3dd72df53cc866afece5802d7e3b4231efb
chain <klaus.mika1052@gmail.com>
2023-04-16 16:37:30 -0300

    adicionado formlário de na pagina de cadastro

--- src/pages/Login/LoginCreate.js
+++ src/pages/Login/LoginCreate.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React from 'react'
+import Input from '../../Components/Forms/Input/Input'
+import useForm from '../../Hooks/useForm'
+import Button from '../../Components/Forms/Button/Button'
 
 const LoginCreate = () => {
-  return <div>LoginCreate</div>;
-};
+  const username = useForm()
+  const email = useForm()
+  const password = useForm()
 
-export default LoginCreate;
+  const handleSubmit = async event => {
+    event.preventDefault()
+  }
+
+  return (
+    <section className="animaLeft">
+      <h1 className="title">Cadastra-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="text" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
+  )
+}
+
+export default LoginCreate
