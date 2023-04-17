93f116fb786d255376ad7fe7b2eec3dbd65a58fa
chain <klaus.mika1052@gmail.com>
2023-04-17 13:25:01 -0300

    criado as rotas da pagina de usurio

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import UserHeader from './UserHeader/UserHeader'
+import Feed from '../../Components/Feed/Feed'
+import UserPhotoPost from './UserPhotoPost/UserPhotoPost'
+import UserStats from './UserStats/UserStats'
+import { Route, Routes } from 'react-router-dom'
 
 const User = () => {
-  return <div>User</div>
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  )
 }
 
 export default User
